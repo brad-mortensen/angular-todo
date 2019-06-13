161a8f3c5fbd01fa1f178445d1e21ca3c631966f
Brad Mortensen <b.mort86@gmail.com>
2019-06-13 17:03:21 -0600

    builds project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  title:string = 'angular-todo';
+
+  constructor() {
+  }
 }
